93e63b5520857038d7c83afc3d89c4f543d67f1e
Yusba <yusba50@gmail.com>
2023-01-31 13:49:37 +0100

    first commit

---
+++ hangman_words.py
@@ -0,0 +1,6 @@
+word_list = [
+  "Branda", "Allan", "Aghonebaren", "Vincitore", "Elohor", "Adaeze", "Yusuf",
+  "irfaan", "Mitchell", "feranmi", "becky", "faith", "victor", "Eboobae",
+  "betty", "delilah", "tochi", "myles", "seven"
+  "king", "Sophia", "Emmanuel", "Josef", "Benny"
+]
